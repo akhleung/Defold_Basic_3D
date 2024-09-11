e7f1e97018a2aba4c4563a75662e6e5035e87307
Aaron Leung <modus.tollens@gmail.com>
2024-09-12 01:53:53 +0200

    fixes and tweaks

---
+++ render/render_state.lua
@@ -0,0 +1,10 @@
+return {
+    WIDTH = 0,
+    HEIGHT = 0,
+    MTX_VIEW = vmath.matrix4(),
+    CAM_URL = "",
+    CAM_POS = vmath.vector4(),
+    BILLBOARD_NORMAL = vmath.vector4(0, 0, 1, 0),
+    KERNEL = {},
+    NOISE = {},
+}
